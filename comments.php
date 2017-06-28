79d7f4619c8ba7c9b0dcf02ab837a4fdcb506761
Bill Sullivan <sulli219@gmail.com>
2017-06-28 09:24:26 -0400

    Added styles for comments

--- comments.php
+++ comments.php
@@ -37,7 +37,7 @@ if ( post_password_required() ) {
 				} else {
 					printf( // WPCS: XSS OK.
 						/* translators: 1: comment count number, 2: title. */
-						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'sdtheme' ) ),
+						esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'sdtheme' ) ),
 						number_format_i18n( $comment_count ),
 						'<span>' . get_the_title() . '</span>'
 					);
@@ -62,6 +62,7 @@ if ( post_password_required() ) {
 				wp_list_comments( array(
 					'style'      => 'ol',
 					'short_ping' => true,
+                    'avatar_size'=> '96,'
 				) );
 			?>
 		</ol><!-- .comment-list -->
