b63db9524f047369d5d55511868476388490f639
Bill Sullivan <sulli219@gmail.com>
2017-06-28 10:00:53 -0400

    Added featured image option to single posts and styled

--- functions.php
+++ functions.php
@@ -41,6 +41,7 @@ function sdtheme_setup() {
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 	 */
 	add_theme_support( 'post-thumbnails' );
+    add_image_size( 'sdtheme-full-bleed', 2000, 12000, true );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
